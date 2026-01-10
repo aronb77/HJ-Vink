d56bfa8f41de0f532f870fe320e3a8d1f3f5f810
BollyDAM <145502155+BollyDAM@users.noreply.github.com>
2026-01-10 10:59:40 +0100

    Fix SVG styling conflicts in Logo component

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,15 +1,217 @@
 import React from "react";
-import Image from "next/image";
 
 export default function Logo({ className }: { className?: string }) {
     return (
-        <Image
-            src="/logo.svg"
-            alt="H.J. Vink Afbouw"
-            width={185}
-            height={91}
+        <svg
+            id="Laag_1"
+            data-name="Laag 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 184.61 90.74"
             className={className}
-            priority
-        />
+            aria-label="H.J. Vink Afbouw Logo"
+        >
+            <defs>
+                <style>{`
+          .v-cls-1 {
+            stroke: url(#v-grad-834);
+          }
+          .v-cls-1, .v-cls-2, .v-cls-3, .v-cls-4, .v-cls-5, .v-cls-6, .v-cls-7, .v-cls-8, .v-cls-9, .v-cls-10 {
+            stroke-miterlimit: 10;
+            stroke-width: 10.51px;
+          }
+          .v-cls-1, .v-cls-2, .v-cls-7, .v-cls-10 {
+            fill: #d7b700;
+          }
+          .v-cls-2 {
+            stroke: url(#v-grad-895);
+          }
+          .v-cls-3 {
+            stroke: url(#v-grad-426);
+          }
+          .v-cls-3, .v-cls-4, .v-cls-5, .v-cls-6, .v-cls-8, .v-cls-9 {
+            fill: none;
+          }
+          .v-cls-4 {
+            stroke: url(#v-grad-938);
+          }
+          .v-cls-5, .v-cls-10 {
+            stroke: #d4af37;
+          }
+          .v-cls-6 {
+            stroke: url(#v-grad-933);
+          }
+          .v-cls-7 {
+            stroke: url(#v-grad-846);
+          }
+          .v-cls-11 {
+            fill: #8c8c8c;
+            font-family: Swiss721BT-BlackExtended, 'Swis721 BlkEx BT';
+            font-size: 12.65px;
+            font-weight: 800;
+          }
+          .v-cls-12 {
+            fill: #d4af37;
+          }
+          .v-cls-8 {
+            stroke: #8c8c8c;
+          }
+          .v-cls-13 {
+            fill: url(#v-grad-779);
+          }
+          .v-cls-9 {
+            stroke: url(#v-grad-910);
+          }
+        `}</style>
+                <linearGradient
+                    id="v-grad-933"
+                    data-name="Naamloos verloop 933"
+                    x1="30.81"
+                    y1="67.33"
+                    x2="57.71"
+                    y2="67.33"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop offset="0" stopColor="#6a6969" />
+                    <stop offset=".11" stopColor="#757474" />
+                    <stop offset=".29" stopColor="#828181" />
+                    <stop offset=".53" stopColor="#898989" />
+                    <stop offset="1" stopColor="#8c8c8c" />
+                </linearGradient>
+                <linearGradient
+                    id="v-grad-426"
+                    data-name="Naamloos verloop 426"
+                    x1="65.82"
+                    y1="44.6"
+                    x2="127.68"
+                    y2="44.6"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop offset="0" stopColor="#121212" />
+                    <stop offset="1" stopColor="#555556" />
+                </linearGradient>
+                <linearGradient
+                    id="v-grad-834"
+                    data-name="Naamloos verloop 834"
+                    x1="107.59"
+                    y1="60.28"
+                    x2="118.1"
+                    y2="60.28"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop offset="0" stopColor="#bb9b31" />
+                    <stop offset="1" stopColor="#d4af37" />
+                </linearGradient>
+                <linearGradient
+                    id="v-grad-779"
+                    data-name="Naamloos verloop 779"
+                    x1="107.58"
+                    y1="35.61"
+                    x2="118.11"
+                    y2="35.61"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop offset="0" stopColor="#bb9b31" />
+                    <stop offset="1" stopColor="#d4af37" />
+                </linearGradient>
+                <linearGradient
+                    id="v-grad-938"
+                    data-name="Naamloos verloop 938"
+                    x1="34.7"
+                    y1="31.87"
+                    x2="52.44"
+                    y2="31.87"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop offset="0" stopColor="#6a6969" />
+                    <stop offset=".02" stopColor="#6b6a6a" />
+                    <stop offset=".33" stopColor="#7d7d7d" />
+                    <stop offset=".65" stopColor="#888" />
+                    <stop offset="1" stopColor="#8c8c8c" />
+                </linearGradient>
+                <linearGradient
+                    id="v-grad-910"
+                    data-name="Naamloos verloop 910"
+                    x1="0"
+                    y1="51.28"
+                    x2="10.5"
+                    y2="51.28"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop offset="0" stopColor="#6a6969" />
+                    <stop offset="1" stopColor="#8c8c8c" />
+                </linearGradient>
+                <linearGradient
+                    id="v-grad-846"
+                    data-name="Naamloos verloop 846"
+                    x1="120.9"
+                    y1="51.21"
+                    x2="131.4"
+                    y2="51.21"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop offset="0" stopColor="#bb9b31" />
+                    <stop offset="1" stopColor="#d4af37" />
+                    <stop offset="1" stopColor="#1d1d1b" />
+                </linearGradient>
+                <linearGradient
+                    id="v-grad-895"
+                    data-name="Naamloos verloop 895"
+                    x1="156.45"
+                    y1="51.21"
+                    x2="166.95"
+                    y2="51.21"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop offset="0" stopColor="#bb9b31" />
+                    <stop offset="1" stopColor="#d4af37" />
+                </linearGradient>
+            </defs>
+            <path
+                className="v-cls-6"
+                d="M52.44,59.38c.03.54.25,5.24-3.53,8.51-3.69,3.2-8.31,2.52-9.71,2.1-.06-.02-.12-.04-.12-.04-2.35-.73-3.65-1.82-4.39-2.63"
+            />
+            <path
+                className="v-cls-3"
+                d="M68.87,28.45c1.36.97,3.26,2.57,4.86,5.02,1.09,1.66,1.35,2.84,2.87,6.77,1.46,3.75,2.5,6.65,3.45,9.26,1.11,3.07,2.24,6.14,3.36,9.21,1.18,3.25,2.88,7.86,5,13.51,2.47-8.96,4.88-16.18,6.73-21.4,1.38-3.87,2.42-6.53,2.69-7.23,1.93-4.92,4.33-11.04,7.58-17.06,3.67-6.79,5.88-10.97,10.47-15.27,3.5-3.27,6.97-5.32,9.45-6.57"
+            />
+            <path className="v-cls-1" d="M112.85,75.87v-31.19,31.19Z" />
+            <path
+                className="v-cls-13"
+                d="M107.58,44.68c0-.17.01-.44.03-.77.05-.87.33-4.61,3.33-10.55.78-1.55,1.2-2.34,1.98-3.32,1.49-1.86,3.96-3.97,4.89-3.4.37.23.29.74.28,4.12,0,.95.01,1.9,0,2.85,0,.43,0,2.54-.01,6.74,0,.99,0,2.48,0,4.33h-10.51Z"
+            />
+            <line className="v-cls-10" x1="147.6" y1="75.87" x2="147.6" y2="26.55" />
+            <line
+                className="v-cls-10"
+                x1="126.69"
+                y1="28.88"
+                x2="147.13"
+                y2="73.68"
+            />
+            <line className="v-cls-5" x1="180.02" y1="28.88" x2="161.7" y2="61.66" />
+            <line className="v-cls-5" x1="166.43" y1="53.56" x2="180.02" y2="73.21" />
+            <line className="v-cls-8" x1="52.44" y1="59.91" x2="52.44" y2="26.61" />
+            <line className="v-cls-4" x1="52.44" y1="31.87" x2="34.7" y2="31.87" />
+            <line className="v-cls-8" x1="23" y1="26.61" x2="23" y2="75.94" />
+            <line className="v-cls-8" x1="23" y1="51.28" x2="5.25" y2="51.28" />
+            <polyline
+                className="v-cls-12"
+                points="175.46 26.35 184.6 31.44 184.6 26.29 184.61 26.29 175.43 26.31"
+            />
+            <polygon
+                className="v-cls-12"
+                points="175.7 76.2 184.34 70.22 184.34 76.2 175.7 76.2"
+            />
+            <text
+                className="v-cls-11"
+                transform="translate(107.58 87.7)"
+            >
+                <tspan x="0" y="0">
+                    AFBOUW
+                </tspan>
+            </text>
+            <line className="v-cls-9" x1="5.25" y1="75.94" x2="5.25" y2="26.61" />
+            <line className="v-cls-7" x1="126.15" y1="26.55" x2="126.15" y2="75.87" />
+            <line className="v-cls-2" x1="161.7" y1="26.55" x2="161.7" y2="75.87" />
+        </svg>
     );
 }
